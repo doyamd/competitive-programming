a1e8cc96638a428c24a96a109425f688e450075c
doyamd <114391494+doyamd@users.noreply.github.com>
2022-12-21 07:27:59 -0800

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/python-division/
+def div(a,b):
+    print(a//b)
+    print(a/b)
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    div(a,b)
