bd5c688178325f1b3279ed39736b546fd9c0fe75
doyamd <114391494+doyamd@users.noreply.github.com>
2022-12-21 07:05:09 -0800

    Create Arithimetic Operaton.py

---
+++ Arithimetic Operaton.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/
+def cal(a,b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    cal(a,b)
