ae93a03c86660b93637c28635786cae3a5c6588f
doyamd <114391494+doyamd@users.noreply.github.com>
2023-01-04 08:00:55 -0800

    Create Building Array from permutation.py

---
+++ Building Array from permutation.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/build-array-from-permutation/description/
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
