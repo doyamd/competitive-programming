69ed16e99a555e7e79fa0442ef5453455844cc4c
doyamd <114391494+doyamd@users.noreply.github.com>
2023-01-17 22:57:48 -0800

    Time: 2356 ms (5.01%), Space: 14.4 MB (84.54%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,12 +1,10 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(1,len(heights)):
-            maxi=heights[i]
-            while heights[i-1]< heights[i] and i>0:
-
-                heights[i-1],heights[i]=heights[i],heights[i-1]
-                names[i-1],names[i]=names[i],names[i-1]
-                i-=1
-                    
+        
+    #Bubble Sorting
+        for i in range(len(heights)):
+            for j in range(len(heights)-1):
+                if heights[j]<heights[j+1]:
+                    heights[j],heights[j+1]=heights[j+1],heights[j]
+                    names[j],names[j+1]=names[j+1],names[j]
         return names
-                
