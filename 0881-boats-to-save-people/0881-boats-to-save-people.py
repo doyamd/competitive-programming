f650c921bff0df1b196759b99cc2e552cd484743
doyamd <114391494+doyamd@users.noreply.github.com>
2023-01-25 07:27:22 -0800

    Time: 671 ms (35.33%), Space: 20.8 MB (66.89%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,18 +1,11 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        # 1 2 2 3
-        # 3 3 4 5
-        #[3021,13025,18326,27390,28998]
-        #29999
         people.sort()
         i=0
         j=len(people)-1
         boats = 0
-        sums = 0
-        print(people)
         while i <= j:
             if people[i] + people[j] > limit:
-                #print(j ,people[i] + people[j])
                 boats += 1
                 j -= 1
             elif  people[i] + people[j] < limit:
