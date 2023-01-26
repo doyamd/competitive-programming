5b00f10da620b4ce879dd1658918f6e23290602d
doyamd <114391494+doyamd@users.noreply.github.com>
2023-01-26 11:23:32 -0800

    Time: 231 ms (70.84%), Space: 25.3 MB (76.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,11 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        """
         while k >0:
             key = nums.pop()
             nums.insert(0,key)
             k -= 1
-        
+        """
+        if k>len(nums): k = k%len(nums)
+        last = nums[len(nums)-k:]
+        first = nums[:len(nums)-k]
+        i = 0
+        for j in range(len(last)):
+            nums[i] = last[j]
+            i += 1
+        for j in range(len(first)):
+            nums[i] = first[j]
+            i += 1    
         
             
                 
