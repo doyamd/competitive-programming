dfe1bb4e60066a59a669854191a7f40c809fc322
doyamd <114391494+doyamd@users.noreply.github.com>
2023-02-23 05:49:22 -0800

    Time: 336 ms (24.62%), Space: 18.4 MB (8.28%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         ans = 0
-        pre = []
-        pre.append(nums[0])
-        for i in range(1,len(nums)):
-            pre.append(pre[-1]+nums[i])
+        pre = 0
+        
         d = defaultdict(int)
         d[0] = 1
-        
-        for i in pre:
-            ans += d[i-k]
-            d[i] += 1 
+        for i in nums:
+            pre += i
+            ans += d[pre-k]
+            d[pre] += 1 
         return ans
                 
             
