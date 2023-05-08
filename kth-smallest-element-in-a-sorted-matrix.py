ddb3b1eba75acacb324cfbfb68419f5dcb9cd31d
doyamd <114391494+doyamd@users.noreply.github.com>
2023-05-08 02:42:00 -0800

    added kth-smallest-element-in-a-sorted-matrix.python

--- kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix.py
@@ -1,23 +1,14 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        queue = deque([(0,0)])
-        visited = set()
-        direc = [[0,1]]
-        def inbound(i,j):
-            if i < 0 or j >= len(matrix) or j < 0 or i >= len(matrix):
-                return False
-            else:
-                return True
-        count = 0
-        while queue:
-            i,j = queue.popleft()
-            count += 1
-            if count == k:
-                return matrix[i][j]
-            if (i,j) not in visited:
-                visited.add((i,j))
-                for x,y in direc:
-                    if inbound(i+x,j+y):
-                        queue.append((i+x,j+y))
-                    elif inbound(i+1,0):
-                        queue.append((i+1,0))
+        if len(matrix) == 1:
+            return matrix[0][k-1]
+        heap = []
+        for i in range(len(matrix)):
+            heappush(heap,(matrix[i][0],i,0))
+        while heap:
+            num, i,j = heappop(heap)
+            k -= 1
+            if not k:
+                return num
+            if (j + 1) < len(matrix):
+                heappush(heap,(matrix[i][j+1],i,j+1))
