8cd69a15fa2b35b7adb7cb0d2817e9f4b54a81b2
doyamd <114391494+doyamd@users.noreply.github.com>
2023-05-16 13:21:35 -0800

    added find-if-path-exists-in-graph.python

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,18 +1,30 @@
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        d= defaultdict(list)
-        for i, j in edges:
-            d[i].append(j)
-            d[j].append(i)
-        visited = set()
-        visited.add(source)
-        def dfs(node):
-            if node == destination:
-                return True
-            lis = d[node]
-            visited.add(node)
-            for i in lis:
-                if i not in visited and dfs(i):
-                   return True
-                
-        return dfs(source)
+    def validPath(self, n: int, edges: List[List[int]], s: int, des: int) -> bool:
+        def representative(x):
+            temp = x
+            while d[temp] != d[d[temp]]:
+                temp = d[temp]
+            return d[temp]
+		
+        def union(x, y):
+            repx = representative(x)
+            repy = representative(y)
+            if rank[repx] >= rank[repy]:
+                d[repx] = repy
+                rank[repy] += rank[repx]
+            else:
+                d[repy] = repx
+                rank[repx] += rank[repy]
+            pass
+
+        def connected(x, y):
+            return representative(y) is representative(x)
+
+        d = defaultdict(int)
+        rank = [1 for _ in range(n)]
+        for i in range(n):
+            d[i] = i
+        for x,y in edges:
+            union(x,y) 
+        
+        return connected(s,des)
