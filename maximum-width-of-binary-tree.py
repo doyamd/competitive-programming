af847ae14357c67b19dae8118e4af00091b69cde
Doi Amdissa <114391494+doyamd@users.noreply.github.com>
2023-10-07 11:08:15 -0800

    added maximum-width-of-binary-tree.python

--- maximum-width-of-binary-tree.py
+++ maximum-width-of-binary-tree.py
@@ -8,13 +8,16 @@ class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         ans = 0
         d = defaultdict(list)
-        def traverse(root, val, depth):
-            if not root:
-                return 
+        node = root
+        heap = [(0,1,node)]
+        visited = set()
+        while heap:
+            depth, val, node = heappop(heap)
             d[depth].append(val)
-            traverse(root.left, val*2, depth + 1)
-            traverse(root.right, (val*2) + 1, depth + 1)
-        traverse(root,1,0)
-        for key,val in d.items():
-            ans = max(ans, val[-1]-val[0]+1)
+            if node and node.left:
+                heappush(heap,(depth+1,val*2,node.left))
+            if node and node.right:
+                heappush(heap,(depth+1,(val*2)+1,node.right))
+        for k, v in d.items():
+            ans = max(ans,(v[-1]-v[0])+1)
         return ans
